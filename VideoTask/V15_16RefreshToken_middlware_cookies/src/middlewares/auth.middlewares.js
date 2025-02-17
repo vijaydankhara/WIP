073611302952e33c33026e6c98a519679ce48a23
vijay dankhara <vijaydankhara111@gmail.com>
2025-02-17 10:52:09 +0530

    17-02-2025

---
+++ VideoTask/V15_16RefreshToken_middlware_cookies/src/middlewares/auth.middlewares.js
@@ -0,0 +1,31 @@
+import { ApiError } from "../utils/apiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.model.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const verifyJWT = asyncHandler(async (req, _, next) => {
+    try {
+        const token  = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
+        console.log(token);
+
+        if (!token) {
+            throw new ApiError(401, "Unauthorized request")
+        }
+        
+        const decodedTOken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+
+        const user = await User.findById(decodedTOken?._id).select("-password -refreshToken");
+
+        if (!user) {
+            throw new ApiError(401, "Invalid Access Token")
+        }
+
+        req.user = user;
+        next()
+    } catch (error) {
+        throw new ApiError(401, error?.message || "Invalid access token")
+    }
+})
