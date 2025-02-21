541f57141a7a7ca5d7352bfcb62294d4200b4062
vijay dankhara <vijaydankhara111@gmail.com>
2025-02-21 12:46:03 +0530

    21-02-2025

--- youTubeApp/src/controllers/video.controller.js
+++ youTubeApp/src/controllers/video.controller.js
@@ -10,6 +10,16 @@ import { Like } from "../models/like.model.js";
 import { Comment } from "../models/comment.model.js";
 
 const publishAVideo = asyncHandler(async (req, res) => {
+  // title and description from the request body
+  // title and description are not empty
+  // videoFile and thumbnail uploaded via req.files.
+  // both video and thumbnail is need  else error 
+  // Uploads the video file and thumbnail to Cloudinary.
+  // video and thumbnail were successfully uploaded.
+  // Store data new 
+  // Checks if the video was successfully else error print
+  // Return the uploded video details and api response
+
   const { title, description } = req.body;
 
   if ([title, description].some((field) => field?.trim() === "")) {
@@ -65,7 +75,81 @@ const publishAVideo = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, video, "Video uploaded successfully"));
 });
 
-const getAllVideos = asyncHandler(async (req, res) => {});
+const getAllVideos = asyncHandler(async (req, res) => {
+
+const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query;
+    console.log(userId);
+    const pipeline = [];
+
+    if (query) {
+        pipeline.push({
+            $search: {
+                index: "search-videos",
+                text: {
+                    query: query,
+                    path: ["title", "description"]
+                }
+            }
+        });
+    }
+
+    if (userId) {
+        if (!isValidObjectId(userId)) {
+            throw new ApiError(400, "Invalid userId");
+        }
+
+        pipeline.push({
+            $match: {
+                owner: new mongoose.Types.ObjectId(userId)
+            }
+        });
+    }
+    pipeline.push({ $match: { isPublished: true } });
+    if (sortBy && sortType) {
+        pipeline.push({
+            $sort: {
+                [sortBy]: sortType === "asc" ? 1 : -1
+            }
+        });
+    } else {
+        pipeline.push({ $sort: { createdAt: -1 } });
+    }
+
+    pipeline.push(
+        {
+            $lookup: {
+                from: "users",
+                localField: "owner",
+                foreignField: "_id",
+                as: "ownerDetails",
+                pipeline: [
+                    {
+                        $project: {
+                            username: 1,
+                            "avatar.url": 1
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            $unwind: "$ownerDetails"
+        }
+    )
+
+    const videoAggregate = Video.aggregate(pipeline);
+
+    const options = {
+        page: parseInt(page, 10),
+        limit: parseInt(limit, 10)
+    };
+
+    const video = await Video.aggregatePaginate(videoAggregate, options);
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, video, "Videos fetched successfully"));
+});
 
 const getVideoById = asyncHandler(async (req, res) => {});
 
